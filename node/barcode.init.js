cac992f042994ede7821390a2d0c967ce40a3954
Tyler Wiseman <wisemantyr@gmail.com>
2021-02-10 19:01:53 -0500

    initial code upload

---
+++ node/barcode.init.js
@@ -0,0 +1,14 @@
+var API_SERVICE = "https://api-na.hosted.exlibrisgroup.com/almaws/v1/";
+var API_REDIRECT = "redirect.js";
+
+//Location validation Regular Expression
+var LOC_REGEX = /^main$/;
+
+//Location format message
+var LOC_MSG = "Invalid Location";
+
+//Barcode validation Regular expression
+var BARCODE_REGEX = /^[0-9\-]{5,14}$/;
+
+//Barcode Format message
+var BARCODE_MSG = "Enter a 5-14 digit barcode containing only numbers and dashes";
---
+++ node/redirect.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const express = require('express');
+const fs = require('fs');
+var prop = require('./prop');
+
+var APIKEY="";
+var regex = /^ALMA_APIKEY=(\S+)\s*$/;
+var lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream(prop.proppath)
+})
+  .on("line", function(line){
+    var match = regex.exec(line);
+    if (match != null) {
+      APIKEY=match[1];
+    }
+  }
+);
+
+
+// Constants
+const PORT = 80;
+const HOST = '0.0.0.0';
+
+// https://github.com/request/request
+const app = express();
+app.get('/redirect.js*', function (req, res) {
+  var v = req.query.apipath;
+  var qs = {
+    "apikey": APIKEY
+  };
+  for(var k in req.query) {
+    if (k == "apipath") continue;
+    qs[k] = req.query[k];
+  }
+  const request = require('request');
+  var opts = {
+    method: 'GET',
+    url: v,
+    qs: qs,
+    headers: {
+      'Accept': 'application/json'
+    }
+  };
+  request(opts).pipe(res);
+});
+app.get('/*', function (req, res) {
+  var url = require('url').parse(req.url);
+  res.sendFile( __dirname + "/" + url.pathname );
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
